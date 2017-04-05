219e6a25dd155dd7b03264c21a0350dc5bd062f8
davemflick <davemflick@yahoo.com>
2017-04-05 17:09:15 -0400

    JSON call made, just playing with json data

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,36 @@
-console.log('Yo')
+const width = 800;
+const height = 350;
+const padding = 40;
+
+var svgContainer = d3.select('.graphContainer')
+						.append('svg')
+						.attr('id', 'svgCont')
+						.attr('width', width + padding*2)
+						.attr('height', height + padding*2);
+
+
+var dataSet = d3.select('.graphContainer')
+					.select('.dataSet')
+					.style('width', width + padding*2);
+
+var visual = svgContainer.append('dataPoint')
+							.attr('id', 'visual')
+							.attr('transform',
+                                  'translate(' + padding + ','
+                                               + padding + ')');
+
+const URL = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+d3.json(URL, (error, data) => {
+	if(error) {
+		return console.error(error)
+	};
+	var dates = [];
+	var GDP = [];
+	var datArr = data.data;
+	datArr.forEach((arg) => {dates.push(arg[0])})
+	datArr.forEach((arg) => {GDP.push(arg[1])})
+	var GDPMIN = d3.min(data.data)
+	var GDPMAX = d3.sum(data.data)
+	console.log(dates, GDP);
+})
+
